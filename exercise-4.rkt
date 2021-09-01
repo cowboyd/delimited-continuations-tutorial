04199018f82aa9b27dda05f1aec6decb08f9dbfa
Charles Lowell <cowboyd@frontside.io>
2021-09-01 14:33:51 +0300

    remove whitespace

--- exercise-4.rkt
+++ exercise-4.rkt
@@ -7,13 +7,12 @@
                       (match list
                         ; the empty list is the recursive base case
                         [(list) 1]
-                        
+
                         ; when the head of any list is 0, just shift immediately
                         ; and discard the rest of the computation and produce 0
                         [(list-rest head rest) #:when (= head 0) (shift _ 0)]
-                        
+
                         ; recursive step
                         [(list-rest head rest) (* head (times rest))])))
 
 (times `(1 0 2 2 3))
-
