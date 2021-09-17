3926ca4ee1c4d11961c73a44627a3b3e8091f39d
Charles Lowell <cowboyd@frontside.io>
2021-09-17 15:50:42 +0300

    Get useState actually working

--- use-state.ts
+++ use-state.ts
@@ -1,5 +1,24 @@
 import { reduce, reset, shift, Prog } from './src';
 
+let component = reduce(function*() {
+  return yield* render(function*() {
+    let [clicks, setClicks] = yield* useCounter();
+    let [strokes, setStrokes] = yield* useState(0);
+    return {
+      clicks,
+      strokes,
+      onClick: () => setClicks(clicks + 1),
+      onKeydown: () => setStrokes(strokes + 1)
+    };
+  })
+})
+
+console.log(component.
+  onClick().
+  onClick().
+  onClick().
+  onKeydown().
+  onKeydown());
 
 function* useState<T>(initial: T): Prog {
   return yield shift(function*(k) {
@@ -10,12 +29,17 @@ function* useState<T>(initial: T): Prog {
   });
 }
 
+function* useCounter() {
+  let [count, setCount] = yield* useState(0);
+  return [count, () => setCount(count + 1)];
+}
+
 function* render(body: () => Prog): Prog {
   return yield reset(function* renderLoop() {
     let states = createStates();
     while (true) {
       yield shift(function* (k) {
-        states.rerender = k;
+        states.reset(k);
         return (yield reset(function*() {
           let result = yield *body();
           return () => result;
@@ -25,22 +49,9 @@ function* render(body: () => Prog): Prog {
   })
 }
 
-let component = reduce(function*() {
-  return yield* render(function*() {
-    let [count, setCount] = yield* useState(0);
-    return {
-      count,
-      onClick: () => setCount(count + 1)
-    };
-  })
-})
-
-console.log(component.onClick().onClick());
-
 interface States {
   getOrCreate<T>(initial: T): Cell<T>;
-  reset(): void
-  rerender(): any;
+  reset(rerender: () => any): void
 }
 interface Cell<T> {
   get(): T;
@@ -48,13 +59,14 @@ interface Cell<T> {
 }
 
 function createStates(): States {
-  console.log('createStates()');
   let states: Cell<unknown>[] = [];
   let currentIdx = -1;
-
+  let render = () => { };
   return {
-    rerender: () => {},
-    reset: () => currentIdx = -1,
+    reset: (rerender) => {
+      currentIdx = -1;
+      render = rerender;
+    },
     getOrCreate<T>(initial: T): Cell<T> {
       if (++currentIdx + 1 > states.length) {
         let value: T = initial;
@@ -62,7 +74,7 @@ function createStates(): States {
           get: () => value,
           set: (t: T) => {
             value = t;
-            return this.rerender();
+            return render();
           }
         });
       }
